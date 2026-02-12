435b8da3924675106b78e73744a1a75cfd5fb492
Worldline-plugins-1 <dev+WorldlinePlugins@logeecom.com>
2026-02-12 17:01:31 +0000

    Release 1.8.7

--- src/MoptWorldline.php
+++ src/MoptWorldline.php
@@ -22,7 +22,9 @@ use Shopware\Core\Framework\Plugin\Util\PluginIdProvider;
 
 class MoptWorldline extends Plugin
 {
+
     const PLUGIN_NAME = 'MoptWorldline';
+
     const PLUGIN_VERSION = '1.8.7';
 
     /**
